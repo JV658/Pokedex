05f81a8ddf5943ed4431a5a118afb5ae24099cfa
Joshua Vand der Most <joshua.vandermost@cambriancollege.ca>
2022-10-04 18:00:38 -0400

    pushing for student

--- Pokedex/PokedexTableViewController.swift
+++ Pokedex/PokedexTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class PokedexTableViewController: UITableViewController {
 
+    var pokedex = [PokedexInfo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,29 +19,34 @@ class PokedexTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        Task{
+            let results = try await PokeAPI_Helper.fetchPokedex()
+            pokedex = results.results
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        print("pokedex count: \(pokedex.count)")
+        return pokedex.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
         // Configure the cell...
+        print(pokedex[indexPath.row])
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
