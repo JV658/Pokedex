ad9ed7c4c59c602e8b6f446bfb813f372b15028b
Joshua Vand der Most <joshua.vandermost@cambriancollege.ca>
2022-10-04 18:35:57 -0400

    completed helper method with some errors remaining

--- Pokedex/PokeAPI_Helper.swift
+++ Pokedex/PokeAPI_Helper.swift
@@ -7,8 +7,39 @@
 
 import Foundation
 
+enum PokedexResponse{
+    case success([PokedexInfo])
+    case failure(Error)
+}
+
 class PokeAPI_Helper{
     private static let pokedexURL = "https://pokeapi.co/api/v2/pokemon"
     
-    
+    public static func fetchPokedex(callback: @escaping (PokedexResponse)->Void){
+        guard
+            let url = URL(string: pokedexURL)
+        else {
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            
+            if let data = data {
+                do{
+                    let decoder = JSONDecoder()
+                    let pokedexData = try decoder.decode(Pokedex.self, from: data)
+                    callback(.success(pokedexData.results))
+                } catch let err {
+                    print("there was an erro: \(err)")
+                }
+            } else if let error = error {
+                callback(.failure(error))
+            } else {
+                print("some random error occured")
+            }
+        }
+        task.resume()
+    }
 }
